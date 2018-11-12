00a78e4321eeee4bd807ec814178dba94ec26ce2
simon <simon@10.50.139.27>
2018-11-12 13:36:33 +0100

    Oprettet Admin

---
+++ src/Data/Admin.java
@@ -0,0 +1,10 @@
+package Data;
+
+import Data.User;
+
+public class Admin extends User {
+
+    public Admin(String navn, String email, String phonenumber, String ID, int birthdate, int memberSince) {
+        super(navn, email, phonenumber, ID, birthdate, memberSince);
+    }
+}
