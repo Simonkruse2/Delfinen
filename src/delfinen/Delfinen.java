a4401fa9f16526983fe2068dfa321351e3c73753
simon <simon@10.50.139.27>
2018-11-12 13:46:07 +0100

    Leget i main

--- src/delfinen/Delfinen.java
+++ src/delfinen/Delfinen.java
@@ -15,7 +15,7 @@ public class Delfinen {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Tunnelsnakes rule!");
+         System.out.println("Tunnelsnakes rule!");
 
     }
     
