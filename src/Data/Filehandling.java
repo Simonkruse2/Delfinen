60af2664e20e03ccc36b0652fff85bd31448805b
simon <simon@10.50.139.27>
2018-11-14 09:47:58 +0100

    testet removeUser();

--- src/Data/Filehandling.java
+++ src/Data/Filehandling.java
@@ -11,52 +11,67 @@ import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
 public class Filehandling {
-    
+
     public static final boolean DEBUG = true;
-    String inFilename = "C:\\Users\\simon\\Documents\\NetBeansProjects\\Delfinen\\files\\teams.txt";
-    String outFilename = "C:\\Users\\simon\\Documents\\NetBeansProjects\\Delfinen\\files\\teams.txt";
-    
-    public void writeObject(ArrayList<User> juniorElite){
+    String inFilename = "C:\\Users\\simon\\Documents\\NetBeansProjects\\Delfinen\\files\\members.txt";
+    String outFilename = "C:\\Users\\simon\\Documents\\NetBeansProjects\\Delfinen\\files\\members.txt";
+
+    public void writeObject(ArrayList<User> members) {
         ObjectOutputStream out = null;
         try {
-            System.out.println(juniorElite);
+            System.out.println(members);
             File file = new File(outFilename);
             out = new ObjectOutputStream(new FileOutputStream(file));
-            out.writeObject(juniorElite);
+            out.writeObject(members);
             out.flush();
         } catch (FileNotFoundException ex) {
-            if(DEBUG) ex.printStackTrace();
+            if (DEBUG) {
+                ex.printStackTrace();
+            }
         } catch (IOException ex) {
-            if(DEBUG) ex.printStackTrace();
+            if (DEBUG) {
+                ex.printStackTrace();
+            }
         } finally {
             try {
                 out.close();
             } catch (IOException ex) {
-                if(DEBUG) ex.printStackTrace();
+                if (DEBUG) {
+                    ex.printStackTrace();
+                }
             }
         }
     }
-    public ArrayList<User> readObject(){
+
+    public ArrayList<User> readObject() {
         ObjectInputStream in = null;
         try {
             File file = new File(outFilename);
             in = new ObjectInputStream(new FileInputStream(file));
-            ArrayList juniorElite = (ArrayList<User>) in.readObject();
-            return juniorElite;
+            ArrayList members = (ArrayList<User>) in.readObject();
+            return members;
         } catch (FileNotFoundException ex) {
-            if(DEBUG) ex.printStackTrace();
+            if (DEBUG) {
+                ex.printStackTrace();
+            }
         } catch (IOException ex) {
-            if(DEBUG) ex.printStackTrace();
+            if (DEBUG) {
+                ex.printStackTrace();
+            }
         } catch (ClassNotFoundException ex) {
-            if(DEBUG) ex.printStackTrace();
+            if (DEBUG) {
+                ex.printStackTrace();
+            }
         } finally {
             try {
                 in.close();
             } catch (IOException ex) {
-                if(DEBUG) ex.printStackTrace();
+                if (DEBUG) {
+                    ex.printStackTrace();
+                }
             }
         }
         return new ArrayList<User>();
     }
-    
+
 }
