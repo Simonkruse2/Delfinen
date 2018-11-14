e75ba4fc4e58067a53b4b159ebc88a888a18268f
simon <simon@10.50.139.27>
2018-11-14 09:37:41 +0100

    Testet FileHandling metode

--- src/delfinen/Delfinen.java
+++ src/delfinen/Delfinen.java
@@ -19,14 +19,25 @@ public class Delfinen {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-         System.out.println("Tunnelsnakes rule!");
          Filehandling f = new Filehandling();
-         User u = new User("a", "a", "a", "a", 201291, 99, false, true, true);
+         User u = new User("b", "b", "b", "b", 201291, 99, false, true, true);
+         User u1 = new User("b1", "b", "b", "b", 201291, 99, false, true, true);
+         User u2 = new User("b2", "b", "b", "b", 201291, 99, false, true, true);
+         User u3 = new User("b3", "b", "b", "b", 201291, 99, false, true, true);
+         User u4 = new User("b4", "b", "b", "b", 201291, 99, false, true, true);
+         User u5 = new User("b5", "b", "b", "b", 201291, 99, false, true, true);
+         User u6 = new User("b6", "b", "b", "b", 201291, 99, false, true, true);
+         User u7 = new User("b7", "b", "b", "b", 201291, 99, false, true, true);
          DataHandling d = new DataHandling();
-         d.addUser();
+         d.addUser(u);
+         d.addUser(u1);
+         d.addUser(u2);
+         d.addUser(u3);
+         d.addUser(u4);
+         d.addUser(u5);
+         d.addUser(u6);
+         d.addUser(u7);
          f.writeObject(d.getJuniorElite());
-         System.out.println(f.readObject());
-         
     }
   
 }
