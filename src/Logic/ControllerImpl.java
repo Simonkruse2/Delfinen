ba74fb38e3a4937baf4e0ea7f929ba2097f40e24
simon <simon@10.50.137.182>
2018-11-15 15:04:29 +0100

    Flyttet til ControllerKlassen

---
+++ src/Logic/ControllerImpl.java
@@ -0,0 +1,44 @@
+
+package Logic;
+
+import Data.User;
+import Data.DataHandling;
+import Data.Filehandling;
+import java.util.ArrayList;
+public class ControllerImpl implements Controller {
+
+    private ArrayList<User> list = new ArrayList<>();
+    private DataHandling d = new DataHandling();
+    private Filehandling f = new Filehandling();
+    private User user;
+    @Override
+    public void opretBruger() {
+
+        if (user.isElite()) {
+            user = new User(user.getID(), user.getName(), user.getBirthdate(), user.getPhonenumber(), 
+                   user.getEmail(), user.isElite(), user.isActive(), user.isCoach(),user.getMemberSince(), user.getDiscipline());
+        } else {
+            user = new User(user.getID(), user.getName(), user.getBirthdate(), user.getPhonenumber(), 
+                   user.getEmail(), user.isElite(), user.isActive(), user.isCoach(),user.getMemberSince(), null);
+        }
+        d.addUser(user);
+        f.writeObject(d.getMembers());
+    }
+
+
+    @Override
+    public void sletBruger(int ID) {
+        for (int i = 0; i < list.size(); i++) {
+            Object get = list.get(i).getID();
+            if (list.get(i).getID() == ID) {
+                list.remove(i);
+            }
+            f.writeObject(list);
+        }
+    }
+
+    @Override
+    public ArrayList<User> readMemberList() {
+       return f.readObject();
+    }
+}
