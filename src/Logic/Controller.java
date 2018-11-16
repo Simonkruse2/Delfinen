86924176d9d2e7d1be2d71a9b055a58eb00641f4
simon <simon@10.50.137.182>
2018-11-16 09:19:48 +0100

    Små ændringer

--- src/Logic/Controller.java
+++ src/Logic/Controller.java
@@ -5,10 +5,10 @@ import java.util.ArrayList;
 
 public interface Controller {
 
-    public void opretBruger(int ID, String name, String birthdate, String phonenumber, 
+    public void opretBruger(int ID, String name, String birthdate, String phonenumber,
             String email, boolean elite, boolean active, boolean coach, String memberSince, String discipline);
- 
+
     public ArrayList<User> readMemberList();
-    
+
     public void sletBruger(int ID);
 }
