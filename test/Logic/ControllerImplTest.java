03e168cd296f34a85dbbca573cc85e49301cc7ad
vince <vince@10.50.137.110>
2018-11-22 12:44:39 +0100

    Added JUnitTest to Delfinen

--- test/Logic/ControllerImplTest.java
+++ test/Logic/ControllerImplTest.java
@@ -7,6 +7,7 @@ package Logic;
 
 import Data.DataHandling;
 import Data.User;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -21,37 +22,55 @@ import org.junit.Ignore;
  * @author jacobfolkehildebrandt
  */
 public class ControllerImplTest {
+
     DataHandling d = new DataHandling();
     ControllerImpl instance = new ControllerImpl(d);
+
     public ControllerImplTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
 
-    /**
-     * Test of opretBruger method, of class ControllerImpl.
-     */
-    @Ignore
     @Test
-    public void testOpretBruger() {
-        System.out.println("opretBruger");
-        int ID = 0;
+    public void testPriceCalculator() {
+        boolean active = true;
+        int age = 28;
+        double priceExpected = 1600;
+
+        double priceResult = instance.priceCalculator(age, active);
+        assertEquals(priceExpected, priceResult, 0.0);
+
+    }
+
+    @Test
+    public void testCalculateAge() {
+        LocalDate birthdate = LocalDate.parse("1990-10-10");
+        LocalDate currentDate = LocalDate.now();
+        int ageExpected = 28;
+        int ageResult = instance.calculateAge(birthdate, currentDate);
+        assertEquals(ageExpected, ageResult);
+    }
+
+    @Test
+    public void testSletBruger() {
+        System.out.println("sletBruger");
+        int ID = 100;
         String name = "Jacob";
-        String birthdate = "100194";
+        String birthdate = "1990-10-10";
         String phonenumber = "21461869";
         String email = "jacobfolke@hotmail.com";
         boolean elite = false;
@@ -59,55 +78,15 @@ public class ControllerImplTest {
         boolean coach = false;
         String memberSince = "2018-11-16";
         String discipline = null;
-        
-        instance.opretBruger(ID, name, birthdate, phonenumber, email, elite, active, coach, memberSince, discipline);
-        // TODO review the generated test code and remove the default call to fail.
 
-    }
-
-    /**
-     * Test of sletBruger method, of class ControllerImpl.
-     */
-    @Test
-    public void testSletBruger() {
-        System.out.println("sletBruger");
-        int ID = 100;
-        
         ArrayList<User> before = instance.readMemberList();
         System.out.println(before.size());
         instance.sletBruger(ID);
         ArrayList<User> after = instance.readMemberList();
         System.out.println(after.size());
-        assertEquals(before.size()-1, after.size());
-        // TODO review the generated test code and remove the default call to fail.
-        
-        ID = 100;
-        String name = "Jacob";
-        String birthdate = "100194";
-        String phonenumber = "21461869";
-        String email = "jacobfolke@hotmail.com";
-        boolean elite = false;
-        boolean active = false;
-        boolean coach = false;
-        String memberSince = "2018-11-16";
-        String discipline = null;
+        assertEquals(before.size(), after.size());
         
         instance.opretBruger(ID, name, birthdate, phonenumber, email, elite, active, coach, memberSince, discipline);
-    }
-
-    /**
-     * Test of readMemberList method, of class ControllerImpl.
-     */
-    @Ignore
-    @Test
-    public void testReadMemberList() {
-        System.out.println("readMemberList");
-        
-        ArrayList<User> expResult = null;
-        ArrayList<User> result = instance.readMemberList();
-        assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
-    
 }
